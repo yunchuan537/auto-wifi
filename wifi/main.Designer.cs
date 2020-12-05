a97e005c70879aa118c7a992f124587159a4c7dd
yunchuan <904053220@qq.com>
2020-12-05 09:08:14 +0800

    init

---
+++ wifi/main.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace wifi
+{
+    partial class main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.TimerCounter = new System.Windows.Forms.Timer(this.components);
+            this.Ping = new System.Windows.Forms.Label();
+            this.CurrentTime = new System.Windows.Forms.Label();
+            this.WifiList = new System.Windows.Forms.ListBox();
+            this.CurrentWifi = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TimerCounter
+            // 
+            this.TimerCounter.Interval = 1000;
+            this.TimerCounter.Tick += new System.EventHandler(this.TimerCounter_Tick);
+            // 
+            // Ping
+            // 
+            this.Ping.AutoSize = true;
+            this.Ping.Font = new System.Drawing.Font("宋体", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Ping.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.Ping.Location = new System.Drawing.Point(365, 9);
+            this.Ping.Name = "Ping";
+            this.Ping.Size = new System.Drawing.Size(131, 38);
+            this.Ping.TabIndex = 6;
+            this.Ping.Text = "status";
+            // 
+            // CurrentTime
+            // 
+            this.CurrentTime.AutoSize = true;
+            this.CurrentTime.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.CurrentTime.Location = new System.Drawing.Point(16, 290);
+            this.CurrentTime.Name = "CurrentTime";
+            this.CurrentTime.Size = new System.Drawing.Size(133, 30);
+            this.CurrentTime.TabIndex = 7;
+            this.CurrentTime.Text = "当前时间";
+            // 
+            // WifiList
+            // 
+            this.WifiList.FormattingEnabled = true;
+            this.WifiList.ItemHeight = 15;
+            this.WifiList.Location = new System.Drawing.Point(21, 9);
+            this.WifiList.Name = "WifiList";
+            this.WifiList.Size = new System.Drawing.Size(320, 274);
+            this.WifiList.TabIndex = 8;
+            // 
+            // CurrentWifi
+            // 
+            this.CurrentWifi.AutoSize = true;
+            this.CurrentWifi.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.CurrentWifi.ForeColor = System.Drawing.Color.Red;
+            this.CurrentWifi.Location = new System.Drawing.Point(347, 79);
+            this.CurrentWifi.Name = "CurrentWifi";
+            this.CurrentWifi.Size = new System.Drawing.Size(64, 25);
+            this.CurrentWifi.TabIndex = 9;
+            this.CurrentWifi.Text = "wifi";
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(503, 335);
+            this.Controls.Add(this.CurrentWifi);
+            this.Controls.Add(this.WifiList);
+            this.Controls.Add(this.CurrentTime);
+            this.Controls.Add(this.Ping);
+            this.Name = "main";
+            this.Text = "win10断网自动重连-云川";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer TimerCounter;
+        private System.Windows.Forms.Label Ping;
+        private System.Windows.Forms.Label CurrentTime;
+        private System.Windows.Forms.ListBox WifiList;
+        private System.Windows.Forms.Label CurrentWifi;
+    }
+}
+
---
+++ wifi/main.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Collections;
+using System.Threading;
+using System.Net;
+using NativeWifi;
+
+namespace wifi
+{
+    public partial class main : Form
+    {
+
+        private WlanClient.WlanInterface WlanIface;
+        private List<Wlan.WlanAvailableNetwork> NetWorkList = new List<Wlan.WlanAvailableNetwork>();
+        private bool isDealing = false; //处理中
+
+
+        public main()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            WlanClient client = new WlanClient();
+            WlanIface = client.Interfaces[0];//一般就一个网卡，有2个没试过。
+
+            Tick(sender, e);
+            this.TimerCounter.Start();
+        }
+
+        private void TimerCounter_Tick(object sender, System.EventArgs e)
+        {
+            Tick(sender, e);
+        }
+
+        private void Tick(object sender, System.EventArgs e)
+        {
+            this.CurrentTime.Text = DateTime.Now.ToLocalTime().ToString();
+
+            Wlan.WlanAvailableNetwork[] networks = WlanIface.GetAvailableNetworkList(0);
+            foreach (Wlan.WlanAvailableNetwork network in networks)
+            {
+                string SSID = WlanHelper.GetStringForSSID(network.dot11Ssid);
+                if (!SSID.Contains("?") && SSID.Length > 0)
+                {
+                    if (network.flags.HasFlag(Wlan.WlanAvailableNetworkFlags.Connected))
+                    {
+                        this.CurrentWifi.Text = SSID;
+                    }
+                    //如果有配置文件的SSID会重复出现。过滤掉
+                    if (!WifiList.Items.Contains(SSID))
+                    {
+                        WifiList.Items.Add(SSID);
+                        NetWorkList.Add(network);
+                    }
+                }
+                
+            }
+
+            checkConnection();
+        }
+
+        private void checkConnection()
+        {
+            if (isDealing)
+            {
+                return;
+            }
+
+            if (!WlanHelper.IsConnectedInternet())
+            {
+                this.Ping.Text = "无网络";
+                ReconnectWifi();
+                return;
+            }
+
+            try
+            {
+                IPAddress[] addresslist = Dns.GetHostAddresses("www.baidu.com");
+                if (addresslist[0].ToString().Length > 6)
+                {
+                    this.Ping.Text = "畅通";
+                }
+                else
+                {
+                    this.Ping.Text = "--";
+                    ReconnectWifi();
+                }
+            }
+            catch
+            {
+                this.Ping.Text = "无外网";
+                ReconnectWifi();
+            }
+        }
+
+        //重连wifi，分为3步：
+        // ipconfig /release
+        // ipconfig /renew
+        // AutoConnect
+        private void ReconnectWifi()
+        {
+            //如果没有外网，就断开重连
+            isDealing = true;
+            Console.WriteLine("断开网络");
+            WlanHelper.DisConnect();
+            Console.WriteLine("重新获取网络");
+            WlanHelper.ReConnect();
+            Console.WriteLine("自动连接");
+            WlanHelper.SetAutoConnect(this.CurrentWifi.Text);
+            isDealing = false;
+        }
+
+    }
+}
